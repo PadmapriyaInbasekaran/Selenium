28808811476772200c1ad84be152efd708f4121c
PadmapriyaInbasekaran <padmapriya.inbasekaran@atmecs.com>
2020-09-12 17:19:42 +0530

    New commits

--- src/main/java/com/atmecs/selenium/DemoBlaze.java
+++ src/main/java/com/atmecs/selenium/DemoBlaze.java
@@ -1,7 +1,5 @@
 package com.atmecs.selenium;
 import java.util.concurrent.TimeUnit;
-
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
